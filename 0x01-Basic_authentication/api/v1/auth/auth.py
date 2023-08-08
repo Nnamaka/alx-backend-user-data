d574b42985be1af1b0e5f7d9cc7fe9f6eb0d23a7
Nnamaka <nnamaka7@gmail.com>
2023-08-08 17:40:37 +0000

    pep statndard

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,12 @@ from typing import List, TypeVar
 
 class Auth:
     """API authentication."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         return None
