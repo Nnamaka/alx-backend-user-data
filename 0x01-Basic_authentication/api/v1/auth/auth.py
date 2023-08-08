73fe6c3a544a7f33573cae2bc88f50428413e59b
nnamaka <nnamaka7@gmail.com>
2023-08-08 20:15:49 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,27 @@ class Auth:
     """API authentication."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Required paths
+        Args:
+            - path(str): Url path
+            - excluded_paths(List of str): List of allowed paths
+        Return:
+            - True if path is not in excluded_paths, else False
+        """
+
+        if path is None:
+            return True
+        
+        # make 'path' forward slash tolerant
+        if path[-1] != '/':
+            path += '/'
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+        elif path in excluded_paths:
+            return False
+        
         return False
 
     def authorization_header(self, request=None) -> str:
