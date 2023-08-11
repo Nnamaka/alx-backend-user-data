02cf7b2f6d0d3b162e0409d5bfe43204dc08063a
nnamaka <nnamaka7@gmail.com>
2023-08-11 10:46:32 +0100

    first task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,6 +42,7 @@ def before_request_func():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
