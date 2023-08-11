3a3e1cee560a651623cea2df38239f74ad5fe1a6
nnamaka <nnamaka7@gmail.com>
2023-08-11 11:49:52 +0100

    the fourth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,7 +7,6 @@ from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
 
-
 class SessionAuth(Auth):
     """ SessionAuth with Auth as base class """
     user_id_by_session_id = {}
@@ -18,8 +17,17 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-
+        
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a user ID based on a session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        
+        return self.user_id_by_session_id.get(session_id)
