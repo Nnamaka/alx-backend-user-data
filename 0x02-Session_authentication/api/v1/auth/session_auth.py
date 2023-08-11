c9252c4ed6ae8a82336a6b692bc4f8fc247f1603
Nnamaka <nnamaka7@gmail.com>
2023-08-11 10:24:27 +0000

    enter correction

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
 
+
 class SessionAuth(Auth):
     """ SessionAuth with Auth as base class """
     user_id_by_session_id = {}
@@ -17,7 +18,7 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
